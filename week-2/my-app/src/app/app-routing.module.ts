bb3720266f2bd8df93ef37955cefac06c92cc428
Emily Richter <emrichter1996@gmail.com>
2020-07-18 13:35:46 -0500

    Week 2; my-app exercise application

---
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+/**
+ * Title: app-routing.module.ts
+ * Author: Emily Richter
+ * Date: 18 July 2020
+ * Description: Routing file
+ */
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path:"",
+    component: HomeComponent
+  },
+  {
+    path:"home",
+    component: HomeComponent
+  },
+  {
+    path:"contact",
+    component: ContactComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
