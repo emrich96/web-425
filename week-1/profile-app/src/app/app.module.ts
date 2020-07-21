795a8fa72a14aeacee0578d11e2bb73390d7f41e
Emily Richter <emrichter1996@gmail.com>
2020-07-21 15:44:15 -0500

    Week 3 composer-app copy; code attribution

--- week-1/profile-app/src/app/app.module.ts
+++ week-1/profile-app/src/app/app.module.ts
@@ -1,3 +1,10 @@
+/**
+ * Title: app.module.ts
+ * Author: Emily Richter
+ * Date: 11 July 2020
+ * Description: App module
+ */
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
