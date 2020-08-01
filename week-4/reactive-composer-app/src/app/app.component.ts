6472de3725626aa5770d34bda07756b90ea8cc4e
Emily Richter <emrichter1996@gmail.com>
2020-08-01 01:42:46 -0500

    Exercise 4.3

---
+++ week-4/reactive-composer-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Title: app.component.ts
+ * Author: Emily Richter
+ * Date: 1 August 2020
+ * Description: App component
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment: string = "Exercise 4.3 - Handling Events with Observables";
+}
---
+++ week-4/reactive-composer-app/src/app/composer.interface.ts
@@ -0,0 +1,12 @@
+/**
+ * Title: composer.interface.ts
+ * Author: Emily Richter
+ * Date: 21 July 2020
+ * Description: Composer interface
+ */
+
+export interface IComposer {
+  composerId: number;
+  fullName: string;
+  genre: string;
+}
