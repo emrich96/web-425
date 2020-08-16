fd92f8431977cb87d267bee581f86e816e3489dd
Emily Richter <emrichter1996@gmail.com>
2020-08-15 21:26:26 -0600

    Assignment 6.4

---
+++ week-6/gpa-calculator-app2/src/app/transcript.interface.ts
@@ -0,0 +1,11 @@
+/**
+ * Title: transcript.interface.ts
+ * Author: Emily Richter
+ * Date: 15 August 2020
+ * Description: Transcript interface
+ */
+
+export interface ITranscript {
+  course: string;
+  grade: string;
+}
